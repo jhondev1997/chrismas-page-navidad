5e1037f7c463d66fb565a838f958ed0ac28f07db
the-user-name <test@email.com>
2021-12-08 19:03:33 -0500

    Probe diferentes formas js variables

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
+let busqueda = document.querySelector("#search-btn");
+let formBusqueda = document.querySelector(".search-form")
 
-let busqueda = document.querySelector('[busqueda]');
-let formBusqueda = document.querySelector('[form-busqueda]');
+
+// let busqueda = document.querySelector('[busqueda]');
+// let formBusqueda = document.querySelector('[form-busqueda]');
 
 busqueda.onclick=()=>{
     busqueda.classList.toggle('fa-times');
-    formBusqueda.classList.toggle('active');
+    formBusqueda.classList.toggle('active1');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active2');
 }
 
+let menu = document.querySelector('[menu]');
+let navbar = document.querySelector('[navbar]');
+
+menu.onclick=()=>{
+    
+    busqueda.classList.remove('fa-times');
+    formBusqueda.classList.remove('active1');
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active2');
+}
 // searchBtn.onclick= ()=>{
 //     searchBtn.classList.toggle('fa-times');
 //     searchForm.classList.toggle('active');
@@ -27,3 +42,10 @@ busqueda.onclick=()=>{
 //     })
     
 // });
+
+window.onscroll = ()=>{
+    busqueda.classList.remove('fa-times');
+    formBusqueda.classList.remove('active1');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active2');
+}
